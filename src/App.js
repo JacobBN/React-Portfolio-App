b462cb16e4a2d0edd967ba7e963f2bea300e1e01
JacobBN <jacob.b.norgaard@gmail.com>
2019-07-17 22:04:32 -0600

    Added Router in App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ function Home() {
   return (
     <>
       <React.StrictMode>
-        <NavBar/>
-        <div className="content">
-
-        </div>
+        <Router>
+          <NavBar/>
+          <Switch>
+            <Route exactly component={Home} pattern="/" />
+            <Route exactly component={Page1} pattern="/path1" />
+            <Route exactly component={Page2} pattern="/path2" />
+            <Route exactly component={Page3} pattern="/path3" />
+            <Route component={Page404} />
+          </Switch>
+        </Router>
       </React.StrictMode>
     </>
   );
