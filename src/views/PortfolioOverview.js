f6dfaa62061b65a3ef04b92ff0064ad1fb9a14f0
JacobBN <jacob.b.norgaard@gmail.com>
2019-07-26 20:37:03 -0600

    Added general overview for portfolio

--- src/views/PortfolioOverview.js
+++ src/views/PortfolioOverview.js
@@ -1,13 +1,76 @@
 import React from 'react';
-// import Card from 'react-bootstrap/Card';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faReact, faGitAlt } from '@fortawesome/free-brands-svg-icons';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Card from 'react-bootstrap/Card';
 import './style/Portfolio.css';
 
 function PortfolioOverview() {
   return (
     <>
-      <div className="content">
+      <div id="content">
+        <CardDeck className="mb-4">
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Project Name</Card.Title>
+              <Card.Text>Lorem Ipsum Thing A Ma Bobba</Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Created June 2019</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="#" />
+            <Card.Body>
+              <Card.Title>Project Name</Card.Title>
+              <Card.Text>Lorem Ipsum Thing A Ma Bobba</Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Created June 2019</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="#" />
+            <Card.Body>
+              <Card.Title>Project Name</Card.Title>
+              <Card.Text>Lorem Ipsum Thing A Ma Bobba</Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Created June 2019</small>
+            </Card.Footer>
+          </Card>
+        </CardDeck>
+        <CardDeck>
+          <Card>
+            <Card.Img variant="top" src="#" />
+            <Card.Body>
+              <Card.Title>Project Name</Card.Title>
+              <Card.Text>Lorem Ipsum Thing A Ma Bobba</Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Created June 2019</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="#" />
+            <Card.Body>
+              <Card.Title>Project Name</Card.Title>
+              <Card.Text>Lorem Ipsum Thing A Ma Bobba</Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Created June 2019</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="#" />
+            <Card.Body>
+              <Card.Title>Project Name</Card.Title>
+              <Card.Text>Lorem Ipsum Thing A Ma Bobba</Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Created June 2019</small>
+            </Card.Footer>
+          </Card>
+        </CardDeck>
       </div>
     </>
   );
